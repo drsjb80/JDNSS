b8fea812a7e6406069ba1426aa8a494473bb2d2a
Steven Beaty <beatys@ms16142.local>
2017-02-22 10:55:19 -0700

    Removing superfluous files.

--- src/main/java/edu/msudenver/cs/javaln/syslog/GetOS.java
+++
@@ -1,8 +0,0 @@
-public class GetOS
-{
-    public static void main (String args[])
-    {
-	System.out.println (System.getProperty ("os.name").
-	    replaceAll (" ", "") + "-" + System.getProperty ("os.arch"));
-    }
-}

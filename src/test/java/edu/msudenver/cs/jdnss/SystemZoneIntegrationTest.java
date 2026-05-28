3ed33a9f716f89705407f1bc957df9bb40d673f1
drsjb80 <drsjb80@gmail.com>
2026-05-28 16:51:55 -0600

    Add zone-file integration test for oneA fixture

---
+++ src/test/java/edu/msudenver/cs/jdnss/SystemZoneIntegrationTest.java
@@ -0,0 +1,102 @@
+package edu.msudenver.cs.jdnss;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SystemZoneIntegrationTest {
+    private Map<String, Zone> originalZones;
+
+    @Before
+    public void setUp() throws Exception {
+        originalZones = new HashMap<>(getBindZones());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        final Map<String, Zone> liveZones = getBindZones();
+        liveZones.clear();
+        liveZones.putAll(originalZones);
+    }
+
+    @Test
+    public void oneAZoneFileParsesAndAnswersLikeSystemFixture() throws Exception {
+        final BindZone zone = new BindZone("oneA.com");
+        try (InputStream in = new FileInputStream("src/test/system/zone_files/oneA.com")) {
+            final Parser parser = new Parser(in, zone);
+            parser.RRs();
+        }
+
+        final Map<String, Zone> liveZones = getBindZones();
+        liveZones.clear();
+        liveZones.put("oneA.com", zone);
+
+        final Query query = new Query(buildQuery(0x6112, "oneA.com", RRCode.A));
+        query.parseQueries("");
+
+        final byte[] result = new Response(query, true).getBytes();
+
+        Assert.assertEquals(1, readUInt16(result, 4));
+        Assert.assertEquals(1, readUInt16(result, 6));
+        Assert.assertEquals(2, readUInt16(result, 8));
+        Assert.assertEquals(2, readUInt16(result, 10));
+        Assert.assertEquals(0, unsignedByte(result[3]) & 0x0f);
+
+        Assert.assertTrue(containsIpv4(result, 192, 168, 1, 2));
+        Assert.assertTrue(containsIpv4(result, 192, 168, 0, 1));
+        Assert.assertTrue(containsIpv4(result, 192, 168, 0, 2));
+    }
+
+    private static byte[] buildQuery(final int id, final String qName, final RRCode type) {
+        byte[] queryBytes = new byte[] {
+                (byte) ((id >> 8) & 0xff), (byte) (id & 0xff),
+                0x01, 0x00,
+                0x00, 0x01,
+                0x00, 0x00,
+                0x00, 0x00,
+                0x00, 0x00
+        };
+
+        queryBytes = Utils.combine(queryBytes, DnsNameCodec.convertString(qName));
+        queryBytes = Utils.combine(queryBytes, new byte[] {
+                (byte) ((type.getCode() >> 8) & 0xff), (byte) (type.getCode() & 0xff),
+                0x00, 0x01
+        });
+        return queryBytes;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, Zone> getBindZones() throws Exception {
+        final Field bindZonesField = JDNSS.class.getDeclaredField("bindZones");
+        bindZonesField.setAccessible(true);
+        return (Map<String, Zone>) bindZonesField.get(null);
+    }
+
+    private static int readUInt16(final byte[] bytes, final int offset) {
+        return (unsignedByte(bytes[offset]) << 8) | unsignedByte(bytes[offset + 1]);
+    }
+
+    private static int unsignedByte(final byte value) {
+        return value & 0xff;
+    }
+
+    private static boolean containsIpv4(final byte[] bytes, final int a, final int b,
+                                        final int c, final int d) {
+        for (int i = 0; i <= bytes.length - 4; i++) {
+            if (unsignedByte(bytes[i]) == a
+                    && unsignedByte(bytes[i + 1]) == b
+                    && unsignedByte(bytes[i + 2]) == c
+                    && unsignedByte(bytes[i + 3]) == d) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

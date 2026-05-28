33a07c5eecb5e96b202d3afeb905800c6f6a05e3
drsjb80 <drsjb80@gmail.com>
2026-05-28 12:04:42 -0600

    Remove P38 and stop testing TCPThread privates

--- src/test/java/edu/msudenver/cs/jdnss/TCPThreadTest.java
+++ src/test/java/edu/msudenver/cs/jdnss/TCPThreadTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -16,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -32,58 +31,6 @@ public class TCPThreadTest {
         Map<String, Zone> liveZones = getBindZones();
         liveZones.clear();
         liveZones.putAll(originalZones);
-    }
-
-    @Test
-    public void getLength() throws Exception {
-        Socket socket = mock(Socket.class);
-        byte b[] = {0, 0};
-        ByteArrayInputStream inputstream = new ByteArrayInputStream(b);
-        ByteArrayOutputStream outputstream = new ByteArrayOutputStream();
-
-        when(socket.getInputStream()).thenReturn(inputstream);
-        when(socket.getOutputStream()).thenReturn(outputstream);
-
-        TCPThread tt = new TCPThread(socket);
-        P38.call("openStreams", tt);
-
-        Object o = P38.call("getLength", tt);
-        Assert.assertEquals(0, o);
-    }
-
-    @Test
-    public void getLengthNonZero() throws Exception {
-        Socket socket = mock(Socket.class);
-        byte[] b = {0x00, 0x1b};
-        ByteArrayInputStream inputstream = new ByteArrayInputStream(b);
-        ByteArrayOutputStream outputstream = new ByteArrayOutputStream();
-
-        when(socket.getInputStream()).thenReturn(inputstream);
-        when(socket.getOutputStream()).thenReturn(outputstream);
-
-        TCPThread tt = new TCPThread(socket);
-        P38.call("openStreams", tt);
-
-        Object o = P38.call("getLength", tt);
-        Assert.assertEquals(27, o);
-    }
-
-    @Test
-    public void closeStreamsClosesSocketAndStreams() throws Exception {
-        Socket socket = mock(Socket.class);
-        InputStream inputStream = mock(InputStream.class);
-        OutputStream outputStream = mock(OutputStream.class);
-
-        when(socket.getInputStream()).thenReturn(inputStream);
-        when(socket.getOutputStream()).thenReturn(outputStream);
-
-        TCPThread tt = new TCPThread(socket);
-        P38.call("openStreams", tt);
-        P38.call("closeStreams", tt);
-
-        verify(inputStream).close();
-        verify(outputStream).close();
-        verify(socket).close();
     }
 
     @Test
@@ -109,6 +56,7 @@ public class TCPThreadTest {
 
         int declaredLength = Utils.addThem(written[0], written[1]);
         Assert.assertEquals(written.length - 2, declaredLength);
+        verify(socket).close();
     }
 
     @Test
@@ -118,6 +66,8 @@ public class TCPThreadTest {
 
         TCPThread tt = new TCPThread(socket);
         tt.run();
+
+        verify(socket, never()).close();
     }
 
     @SuppressWarnings("unchecked")
